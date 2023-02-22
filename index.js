955b8690f17f650fe8edff15ecb6d864c8032f30
Chiara Arrivas <ritarrivas96@gmail.com>
2023-02-22 11:32:31 +0100

    second commit

--- index.js
+++ index.js
@@ -9,7 +9,14 @@ app.use(cors());
 app.use(helmet());
 
 app.use(express.json({extended: true}));
-app.use(express.url({extended: true}));
+app.use(express.urlencoded({extended: true}));
+
+app.post("/:user_id", (req, res) => {
+   if(req.body.title !== "test1" ) {
+    return res.status(500).json({message: "Error body title"})
+   }
+    return res.status(201).json({status: "online", body: {...req.body} ,query: {...req.query}, params: {...req.params}, headers: {...req.headers}})
+})
 
 const {SERVER_PORT} = process.env;
 
